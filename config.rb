92717aebb83d5d94ead65436bd832ede3ac25c63
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-04-03 12:32:11 +0300

    Add deployment configuration

--- config.rb
+++ config.rb
@@ -13,34 +13,17 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :asset_hash
+  activate :relative_assets
+  activate :minify_css
+  activate :minify_javascript
+end
+
+# Deploy-specific configuration
+activate :gh_pages do |gh_pages|
+  gh_pages.remote = 'https://github.com/partialconf/partialconf.github.io'
+end
